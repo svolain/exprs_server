d63edddf18c0e707cbb88c6bc1956aba4ac8af80
svolain <savoville@gmail.com>
2024-09-30 22:04:14 +0300

    removed non used code from comments

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-/*
-import express from "express";
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
-});*/
-
-
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
